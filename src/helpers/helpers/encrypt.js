d19ed3f56b43d1e16cae8f4cc64e10ff3426cc49
Adson Gomes Olivera <adsonprofessionalemail@gmail.com>
2023-03-27 12:15:45 -0300

    <feat> Added helpers to gateway

---
+++ src/helpers/helpers/encrypt.js
@@ -0,0 +1,10 @@
+const bcryptjs = require('bcryptjs');
+
+const encrypt = async (password) => {
+  const salt = await bcryptjs.genSalt(10);
+  const hashedPassword = await bcryptjs.hash(password, salt);
+
+  return hashedPassword;
+};
+
+module.exports = encrypt;
