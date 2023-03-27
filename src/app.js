a21247a18460fdb5480d2a2a5cd71967a3cbe656
Adson Gomes Olivera <adsonprofessionalemail@gmail.com>
2023-03-27 08:49:35 -0300

    <feat> Created routes for API Gateway

--- src/app.js
+++ src/app.js
@@ -2,15 +2,16 @@ require('express-async-errors');
 const express = require('express');
 const helmet = require('helmet');
 const cors = require('cors');
+const routes = require('./routes');
 
 const app = express();
 
 app.use(helmet());
 app.use(cors());
 
-app.use('/api/products');
-app.use('/api/accounts');
-app.use('/api/orders');
-app.use('/api/payments');
+app.use('/api/products', routes.productsRoutes);
+app.use('/api/accounts', routes.accountsRoutes);
+app.use('/api/orders', routes.ordersRoutes);
+app.use('/api/payments', routes.paymentsRoutes);
 
 module.exports = app;
---
+++ src/routes/index.js
@@ -0,0 +1,11 @@
+const productsRoutes = require('./products.routes');
+const accountsRoutes = require('./accounts.routes');
+const ordersRoutes = require('./orders.routes');
+const paymentsRoutes = require('./payments.routes');
+
+module.exports = {
+  productsRoutes,
+  accountsRoutes,
+  ordersRoutes,
+  paymentsRoutes,
+};
