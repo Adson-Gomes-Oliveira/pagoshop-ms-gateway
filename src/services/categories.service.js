b11d3d3c6cdb32b68e80dc55cbe6a7c9881c4bc3
Adson Gomes Olivera <adsonprofessionalemail@gmail.com>
2023-03-27 12:51:09 -0300

    <feat> Created services for categories

---
+++ src/services/categories.service.js
@@ -0,0 +1,40 @@
+const axios = require('axios');
+
+const getCategories = async () => {
+  const response = await axios.get(`http://${process.env.CATEGORY_HOST}:3001/api/categories/`);
+  return response;
+}
+
+const getOneCategory = async (id) => {
+  const response = await axios.get(`http://${process.env.CATEGORY_HOST}:3001/api/categories/${id}`);
+  return response;
+}
+
+const createCategory = async (payload) => {
+  const response = await axios.post(`http://${process.env.CATEGORY_HOST}:3001/api/categories/`, payload);
+  return response;
+}
+
+const updateCategory = async (id, payload) => {
+  const response = await axios.put(`http://${process.env.CATEGORY_HOST}:3001/api/categories/${id}`, payload);
+  return response;
+}
+
+const updateStatusCategory = async (id, payload) => {
+  const response = await axios.patch(`http://${process.env.CATEGORY_HOST}:3001/api/categories/${id}`, payload);
+  return response;
+};
+
+const deleteCategory = async (id) => {
+  await axios.delete(`http://${process.env.CATEGORY_HOST}:3001/api/categories/${id}`);
+  return null;
+}
+
+module.exports = {
+  getCategories,
+  getOneCategory,
+  createCategory,
+  updateCategory,
+  updateStatusCategory,
+  deleteCategory,
+}
--- src/services/products.service.js
+++ src/services/products.service.js
@@ -25,46 +25,10 @@ const deleteProduct = async (id) => {
   return null;
 }
 
-const getCategories = async () => {
-  const response = await axios.get(`http://${process.env.CATEGORY_HOST}:3001/api/categories/`);
-  return response;
-}
-
-const getOneCategory = async (id) => {
-  const response = await axios.get(`http://${process.env.CATEGORY_HOST}:3001/api/categories/${id}`);
-  return response;
-}
-
-const createCategory = async (payload) => {
-  const response = await axios.post(`http://${process.env.CATEGORY_HOST}:3001/api/categories/`, payload);
-  return response;
-}
-
-const updateCategory = async (id, payload) => {
-  const response = await axios.put(`http://${process.env.CATEGORY_HOST}:3001/api/categories/${id}`, payload);
-  return response;
-}
-
-const updateStatusCategory = async (id, payload) => {
-  const response = await axios.patch(`http://${process.env.CATEGORY_HOST}:3001/api/categories/${id}`, payload);
-  return response;
-};
-
-const deleteCategory = async (id) => {
-  await axios.delete(`http://${process.env.CATEGORY_HOST}:3001/api/categories/${id}`);
-  return null;
-}
-
 module.exports = {
   getProducts,
   getOneProduct,
   createProduct,
   updateProduct,
   deleteProduct,
-  getCategories,
-  getOneCategory,
-  createCategory,
-  updateCategory,
-  updateStatusCategory,
-  deleteCategory,
 }
