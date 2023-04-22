6c50ab49265177385376f03e5ebb5665e0f1184e
Adson Gomes Oliveira <adsongoliveira2022@outlook.com>
2023-04-22 09:52:09 -0300

    <setup> Adding env configs for deploy

--- src/redis/blocklist.js
+++ src/redis/blocklist.js
@@ -1,9 +1,10 @@
 const redis = require('redis');
+require('dotenv').config();
 
 const client = redis.createClient({
   socket: {
     host: process.env.REDIS_HOST || '127.0.0.1',
-    port: '6379',
+    port: process.env.REDIS_PORT || '6379',
   },
   prefix: 'blocklist:',
 });
